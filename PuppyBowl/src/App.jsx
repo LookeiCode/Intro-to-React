43b0840c85df80fb9426e473cfce72877c0ce06f
LookeiCode <137450026+LookeiCode@users.noreply.github.com>
2023-08-28 21:26:56 -0400

    Rendered the puppies to the page

--- PuppyBowl/src/App.jsx
+++ PuppyBowl/src/App.jsx
@@ -1,11 +1,19 @@
 import { useState } from 'react'
+import { puppyList } from './data.js'
 import './App.css'
 
 function App() {
+  const [puppies, setPuppies] = useState(puppyList)
+
+
+  console.log(puppyList)
 
   return (
     <>
-     
+      {puppyList.map((puppy) => {
+        return <p key={puppy.id}>{puppy.name}</p>
+      })
+      }
     </>
   )
 }
